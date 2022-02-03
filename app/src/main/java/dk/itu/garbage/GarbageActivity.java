5af7af1a7733565f5339687a39828fee9341191f
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-02-03 12:08:13 +0100

    Garbage new project set up

---
+++ app/src/main/java/dk/itu/garbage/GarbageActivity.java
@@ -0,0 +1,14 @@
+package dk.itu.garbage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GarbageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
