640e4db4545da0a9724f38d5b0674ce6bf09a571
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-02-11 13:01:10 +0100

    v2 layout, activity, intent setup

---
+++ app/src/main/java/dk/itu/garbage/AddingActivity.java
@@ -0,0 +1,16 @@
+package dk.itu.garbage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adding);
+
+
+    }
+}
--- app/src/main/java/dk/itu/garbage/GarbageActivity.java
+++ app/src/main/java/dk/itu/garbage/GarbageActivity.java
@@ -2,6 +2,7 @@ package dk.itu.garbage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,7 +10,7 @@ import android.widget.TextView;
 
 public class GarbageActivity extends AppCompatActivity {
     //GUI variables
-    private Button listItems;
+    private Button searchItems, toAddItem;
     private TextView item;
 
     private ItemsDB itemDB;
@@ -23,12 +24,19 @@ public class GarbageActivity extends AppCompatActivity {
         itemDB.fillItemsDB();
 
         item = findViewById(R.id.input_text);
-        listItems = findViewById(R.id.where_button);
+        searchItems = findViewById(R.id.where_button);
+
+        toAddItem = findViewById(R.id.to_add_item);
 
         //expression lambda (instead of lambda statement)
-        listItems.setOnClickListener((View v) ->
+        searchItems.setOnClickListener((View searchBtn) ->
             item.setText(itemDB.listItems(item.getText().toString()))
         );
+
+        toAddItem.setOnClickListener((View toAddBtn) -> {
+            Intent intent = new Intent(GarbageActivity.this, AddingActivity.class);
+            startActivity(intent);
+                });
 
 
 
