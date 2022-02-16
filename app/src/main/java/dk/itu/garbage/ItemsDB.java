2b0490827f8bda776962aabc6bcbc5096415a504
ETALATE <84042296+ETALATE@users.noreply.github.com>
2022-02-16 15:22:51 +0100

    changed db from list to map

--- app/src/main/java/dk/itu/garbage/ItemsDB.java
+++ app/src/main/java/dk/itu/garbage/ItemsDB.java
@@ -1,13 +1,15 @@
 package dk.itu.garbage;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ItemsDB {
+    //a static factory method with the return type as an object of this singleton class
     private static ItemsDB sItemsDB;
-    private List<Item> ItemsDB= new ArrayList<>();
+    private Map<String, String> itemsDB = new HashMap<>();
 
+    //declaring access modifier of constructor private
     private ItemsDB() {
         fillItemsDB();
     }
@@ -27,32 +29,27 @@ public class ItemsDB {
     // takes input from TextEdit and searches Item object for equality
     public String searchItems(String input) {
         String dbItem = "";
-        for(Item i: ItemsDB) {
-            if (input.equals(i.getWhat())) {
-               dbItem = i.toString();
-            }
+        if (itemsDB.containsKey(input.toLowerCase().trim())) {
+           dbItem = input + " should be placed in: " + itemsDB.get(input);
+           return dbItem;
         }
 
-        if (dbItem.equals("")) {
-            dbItem = input + " should be placed in: not found";
-        }
-
-        return dbItem;
+        return input + " not found";
     }
 
     public void addItem(String what, String where) {
-        ItemsDB.add(new Item(what, where));
+        itemsDB.put(what, where);
     }
 
     private void fillItemsDB() {
-        ItemsDB.add(new Item("coffee", "Bio"));
-        ItemsDB.add(new Item("carrots", "Bio"));
-        ItemsDB.add(new Item("milk carton", "Residual Waste"));
-        ItemsDB.add(new Item("bread", "Bio"));
-        ItemsDB.add(new Item("butter", "Bio"));
-
-        ItemsDB.add(new Item("peanut butter", "Bio"));
-        ItemsDB.add(new Item("phone", "Electronic Waste"));
+        //get items and categories from file
+        itemsDB.put("coffee", "Bio");
+        itemsDB.put("carrots", "Bio");
+        itemsDB.put("milk carton", "Residual Waste");
+        itemsDB.put("bread", "Bio");
+        itemsDB.put("butter", "Bio");
+        itemsDB.put("peanut butter", "Bio");
+        itemsDB.put("phone", "Electronic Waste");
     }
 
 }
